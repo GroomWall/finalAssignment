ede44075c92ee62625bf3c47831b8bbd0f2ebe08
GroomWall <43686279+GroomWall@users.noreply.github.com>
2020-04-12 01:07:23 +0300

    Logic enhancements

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 let counter = document.getElementsByTagName("label");
 function decrease(){
-	let text = counter[0].innerText;
-	if(parseInt(text,10) <= 0){
-	}else{
-	counter[0].innerText = text - 1;
-	}
+	counter[0].innerText = counter[0].innerText - 1;
 }
 function increase(){
 	counter[0].innerText = parseInt(counter[0].innerText,10) + 1;
